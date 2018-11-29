b04c79d984801a4871667bacaf9ef1f117f99b68
sandeep <sandeep@ashmar.in>
2018-11-29 12:29:01 +0530

    comit change

--- app/src/main/java/com/monet/bbc/activity/LoginScreen.java
+++ app/src/main/java/com/monet/bbc/activity/LoginScreen.java
@@ -1,14 +1,29 @@
 package com.monet.bbc.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.monet.bbc.R;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        findViewById(R.id.tv_login_forgot).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.tv_login_forgot:
+                startActivity(new Intent(this, ResetPasswordScreen.class));
+                break;
+        }
     }
 }
+
---
+++ app/src/main/java/com/monet/bbc/activity/ResetPasswordScreen.java
@@ -0,0 +1,15 @@
+package com.monet.bbc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.monet.bbc.R;
+
+public class ResetPasswordScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password_screen);
+    }
+}
