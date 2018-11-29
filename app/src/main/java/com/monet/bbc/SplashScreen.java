560e838d7da72a03af12fe478648a6ddab07846e
sandeep <sandeep@ashmar.in>
2018-11-29 09:13:30 +0530

    first commit

--- app/src/main/java/com/monet/bbc/SplashScreen.java
+++ app/src/main/java/com/monet/bbc/SplashScreen.java
@@ -1,7 +1,11 @@
 package com.monet.bbc;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.monet.bbc.activity.LoginScreen;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        SplashScreenFinish splashScreenFinish = new SplashScreenFinish(2000, 1000);
+        splashScreenFinish.start();
+
+    }
+
+    class SplashScreenFinish extends CountDownTimer{
+
+        public SplashScreenFinish(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            startActivity(new Intent(SplashScreen.this, LoginScreen.class));
+            finish();
+        }
     }
 }
