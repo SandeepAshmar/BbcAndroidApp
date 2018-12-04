a3ed9bb47f47c2783a9372ebd13ac55cd3b2a796
divyashamar <divya.prakash@ashmar.in>
2018-12-04 12:40:40 +0530

    Some Changes

--- app/src/main/java/com/monet/bbc/fragment/HomeFragment.java
+++ app/src/main/java/com/monet/bbc/fragment/HomeFragment.java
@@ -1,22 +1,26 @@
 package com.monet.bbc.fragment;
 
 
+import android.media.tv.TvContract;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.monet.bbc.R;
+import com.monet.bbc.adapter.SlidingImage_Adapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
-
     public HomeFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -24,7 +28,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        /*viewPager = view.findViewById(R.id.pager);
+        adapter = new SlidingImage_Adapter(this.getActivity(),IMAGES);
+        viewPager.setAdapter(adapter);*/
+        return view;
     }
+
 
 }
