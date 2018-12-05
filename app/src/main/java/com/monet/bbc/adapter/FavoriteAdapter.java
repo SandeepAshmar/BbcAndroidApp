72b54e7b579863a864871671aff3654669993573
divyashamar <divya.prakash@ashmar.in>
2018-12-05 09:19:42 +0530

    Some Changes

---
+++ app/src/main/java/com/monet/bbc/adapter/FavoriteAdapter.java
@@ -0,0 +1,80 @@
+package com.monet.bbc.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.monet.bbc.R;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.ViewHolder> {
+
+    Context context;
+    int size;
+
+    public FavoriteAdapter(Context context, int size) {
+        this.context = context;
+        this.size = size;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_favorite, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        Glide.with(context).load("https://www.serveit.com/media/1207/alan-mac-kenna-1-small.jpg").into(holder.imageView);
+
+        holder.img_favorite_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Share"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.img_favorite_playList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "PlayList"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.img_favorite_love.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Love" + position, Toast.LENGTH_SHORT).show();
+                notifyItemRemoved(position);
+                size = size - 1;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return size;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView, img_favorite_share, img_favorite_playList,img_favorite_love;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.img_favorite_item);
+            img_favorite_share = itemView.findViewById(R.id.img_favorite_share);
+            img_favorite_playList = itemView.findViewById(R.id.img_favorite_playList);
+            img_favorite_love = itemView.findViewById(R.id.img_favorite_love);
+        }
+    }
+}
--- app/src/main/java/com/monet/bbc/fragment/FavouriteFragment.java
+++ app/src/main/java/com/monet/bbc/fragment/FavouriteFragment.java
@@ -3,28 +3,40 @@ package com.monet.bbc.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.monet.bbc.R;
+import com.monet.bbc.adapter.FavoriteAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavouriteFragment extends Fragment {
 
-
-    public FavouriteFragment() {
-        // Required empty public constructor
-    }
-
+    RecyclerView recyclerView;
+    FavoriteAdapter favoriteAdapter;
+    LinearLayoutManager linearLayoutManager;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false);
+        view = inflater.inflate(R.layout.fragment_favourite, container, false);
+
+        recyclerView = view.findViewById(R.id.rv_fav_list);
+
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        favoriteAdapter = new FavoriteAdapter(getActivity(), 50);
+        recyclerView.setAdapter(favoriteAdapter);
+
+
+        return view;
     }
 
 }
