1288b73336ce7bf6641c713217727fa60a9e5b03
sandeep <sandeep@ashmar.in>
2018-12-11 17:01:12 +0530

    comit change

---
+++ app/src/main/java/com/monet/bbc/activity/SimpleVideoPlay.java
@@ -0,0 +1,69 @@
+package com.monet.bbc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.monet.bbc.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SimpleVideoPlay extends AppCompatActivity implements View.OnClickListener{
+
+    private VideoView video_SVP;
+    private RelativeLayout rl_pauseLayout;
+    private TextView tv_videoCurrentTime, tv_videoLength, tv_SVP_videoName, tv_bySomeone_SVP, tv_SVP_posted;
+    private SeekBar sb_SVP;
+    private CircleImageView img_SVP_user;
+    private RecyclerView rv_svp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_video_play);
+
+
+        findViewById(R.id.img_playVideo).setOnClickListener(this);
+        findViewById(R.id.img_SVP_back).setOnClickListener(this);
+        findViewById(R.id.img_SVP_search).setOnClickListener(this);
+        findViewById(R.id.img_fullScreen).setOnClickListener(this);
+        findViewById(R.id.img_dots).setOnClickListener(this);
+        video_SVP = findViewById(R.id.video_SVP);
+        rl_pauseLayout = findViewById(R.id.rl_pauseLayout);
+        video_SVP.setOnClickListener(this);
+        rl_pauseLayout.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.img_playVideo:
+                rl_pauseLayout.setVisibility(View.GONE);
+                Toast.makeText(this, "Video will play again", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_SVP_back:
+                finish();
+                break;
+            case R.id.img_SVP_search:
+                Toast.makeText(this, "search function", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_fullScreen:
+                Toast.makeText(this, "full screen", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_dots:
+                Toast.makeText(this, "dots clik", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.video_SVP:
+                rl_pauseLayout.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/monet/bbc/adapter/HomeShowsAdapter.java
+++ app/src/main/java/com/monet/bbc/adapter/HomeShowsAdapter.java
@@ -1,6 +1,7 @@
 package com.monet.bbc.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.monet.bbc.R;
+import com.monet.bbc.activity.SimpleVideoPlay;
 
 import java.util.ArrayList;
 
@@ -44,6 +46,13 @@ public class HomeShowsAdapter extends RecyclerView.Adapter<HomeShowsAdapter.View
         if(position == 1){
             holder.onlineView.setVisibility(View.VISIBLE);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.startActivity(new Intent(context, SimpleVideoPlay.class));
+            }
+        });
     }
 
     @Override
