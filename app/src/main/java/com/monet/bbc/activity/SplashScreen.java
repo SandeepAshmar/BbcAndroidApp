c9eb76df88197289bacf11cb62c0a1fbeab823fa
sandeep <sandeep@ashmar.in>
2019-01-08 12:30:42 +0530

    some changes

--- app/src/main/java/com/monet/bbc/activity/SplashScreen.java
+++ app/src/main/java/com/monet/bbc/activity/SplashScreen.java
@@ -1,20 +1,15 @@
-package com.monet.bbc;
+package com.monet.bbc.activity;
 
 import android.annotation.SuppressLint;
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.monet.bbc.activity.HomeScreen;
-import com.monet.bbc.activity.LoginScreen;
+import com.monet.bbc.R;
 import com.monet.bbc.utils.AppPreference;
 
 import static com.monet.bbc.utils.AppUtils.checkConnection;
-import static com.monet.bbc.utils.AppUtils.isConnectionAvailable;
-import static com.monet.bbc.utils.AppUtils.shortToast;
 
 public class SplashScreen extends AppCompatActivity {
 
